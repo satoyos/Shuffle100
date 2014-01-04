ae2d8ff1853eba5ccdbbcb80dfbb6d2dc16bd3bc
Yoshifumi Sato <sato_yos@nifty.com>
2014-01-04 20:45:48 +0900

    fixed small bug

--- app/controllers/recite_poem_controller.rb
+++ app/controllers/recite_poem_controller.rb
@@ -1,3 +1,14 @@
 class RecitePoemController < UIViewController
 
+  attr_reader :deck
+
+  def initWithDeck(deck, players: players)
+    self.initWithNibName(nil, bundle: nil)
+    @deck = deck
+  end
+
+  def viewDidLoad
+    super
+
+  end
 end
--- spec/controllers/recite_poem_controller_spec.rb
+++ spec/controllers/recite_poem_controller_spec.rb
@@ -1,7 +1,26 @@
 describe 'RecitePoemController' do
-  tests RecitePoemController
 
   it 'should be a valid controller' do
+    tests RecitePoemController
+
     controller.should.not.be.nil
+    controller.is_a?(RecitePoemController).should.be.true
+  end
+
+  describe '初期化' do
+    context 'initWithFrame:deck:で初期化' do
+      before do
+        @controller = RecitePoemController.alloc
+        @controller.initWithDeck(Deck.new, players: [])
+      end
+
+      it 'should be a valid controller' do
+        @controller.should.not.be.nil
+      end
+
+      it 'プロパティとしてdeckを持つ' do
+        @controller.deck.should.not.be.nil
+      end
+    end
   end
 end
