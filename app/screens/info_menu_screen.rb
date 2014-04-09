3be8498655003afb5c375728465ee9730f63ac21
Yoshifumi Sato <sato_yos@nifty.com>
2014-04-09 20:19:27 +0900

    ヘルプメニューの項目の順序入れ替え
      ・全体の流れが分かる「試合の流れ」を冒頭に移した。

--- app/screens/info_menu_screen.rb
+++ app/screens/info_menu_screen.rb
@@ -6,14 +6,14 @@ class InfoMenuScreen < PM::TableScreen
         cells:
             [
                 {
-                    title: '設定できること',
-                    action: :open_options_help,
-                    accessibility_label: 'open_options_help'
-                },
-                {
                     title: '試合の流れ',
                     action: :open_game_flow_help,
                     accessibility_label: 'open_game_flow_help'
+                },
+                {
+                    title: '設定できること',
+                    action: :open_options_help,
+                    accessibility_label: 'open_options_help'
                 },
                 {
                     title: 'バージョン',
@@ -33,12 +33,10 @@ class InfoMenuScreen < PM::TableScreen
   end
 
   def open_options_help
-#    open SettingsHelpScreen.new
     open InfoScreen.new(url: 'html/options.html', title: '設定できること')
   end
 
   def open_game_flow_help
-#    open GameFlowHelpScreen.new
     open InfoScreen.new(url: 'html/game_flow.html', title: '試合の流れ')
   end
 end
