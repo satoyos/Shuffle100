8be4db2841d6fb99f5a27f848bf63cc383cc46bf
Yoshifumi Sato <sato_yos@nifty.com>
2014-04-26 21:31:14 +0900

    歌検索を行うFrankテストの作成に着手。
      ・検索用TextFieldに文字列を送り込むことはできた。
      ・しかし、それで絞り込みを行った結果が表示されない…。orz

---
+++ features/search_poems.feature
@@ -0,0 +1,19 @@
+Feature:
+  As an 百人一首 player
+  I want to search poems with string
+  So I can easily select poems to use
+
+  Background:
+    Given I launch the app
+
+  Scenario:
+  テキストフィールドに文字を入力すると、歌を絞り込むことができる。
+    Then I should see "百首読み上げ"
+
+    # 「秋」と入力して
+    When I touch the table cell marked "select_poem"
+    When I wait to see "歌を選ぶ"
+    When I wait for 1 second
+    When I touch "search_text_field"
+#    When I type "aki"
+    When I use the keyboard to fill in the textfield marked "search_text_field" with "秋"
--- features/step_definitions/recite_screen_steps.rb
+++ features/step_definitions/recite_screen_steps.rb
@@ -152,3 +152,19 @@ Then /^I can go through (\d+) poems with skip$/ do |poems_num|
   end
   puts "#{Time.now} - [#{poems_num}] Loop End!"
 end
+
+When /^I type "([^\"]*)"$/ do |text_to_type|
+  type_into_keyboard(text_to_type)
+end
+
+When /^I use the keyboard to fill in the textfield marked "([^\\"]*)" with "([^\\"]*)"$/ do |text_field_mark, text_to_type|
+  text_field_selector =  "view marked:'#{text_field_mark}'"
+  check_element_exists( text_field_selector )
+  touch( text_field_selector )
+  frankly_map( text_field_selector, 'setText:', text_to_type )
+  frankly_map( text_field_selector, 'endEditing:', true )
+end
+
+When /^I set Japanese mode$/ do
+
+end
