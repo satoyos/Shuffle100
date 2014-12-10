a68ae9eb20936415984d1cb885ce2a94d4c3bf85
Yoshifumi Sato <sato_yos@nifty.com>
2014-12-10 18:47:17 +0900

    Refactor: PoemPickerの表示データ生成部をPoemPickerDataSourceモジュールに分割した。

---
+++ app/dataSources/poem_picker_data_source.rb
@@ -0,0 +1,55 @@
+module PoemPickerDataSource
+
+  SELECTED_BG_COLOR = '#eebbcb'.uicolor #撫子色
+
+  def table_data
+    [{
+         cell_style: UITableViewCellStyleValue1,
+         cells: poems.map{|poem|
+           {
+               title: '%3d. %s %s %s %s %s' %
+                   ([poem.number] + (0..4).to_a.map{|idx| poem.liner[idx]}),
+
+               subtitle: "　　 #{poem.poet}",
+               search_text: search_text_for_poem(poem),
+               action: :poem_tapped,
+               arguments: {number: poem.number},
+               style: {
+                   # font: UIFont.fontWithName('HiraMinProN-W6', size: 16),
+                   font: UIFont.fontWithName('HiraMinProN-W6', size: MDT::Font.body.pointSize),
+                   background_color: bg_color_for_poem(poem),
+                   accessibility_label: '%03d' % poem.number,
+                   accessory_type: acc_type_for_poem(poem),
+               },
+               height: MDT::Font.body.pointSize * 3
+           }
+         }
+     }]
+  end
+
+  private
+
+  # @param [Poem] poem
+  # @return [UIColor]
+  def bg_color_for_poem(poem)
+    status100.of_number(poem.number) ?
+        SELECTED_BG_COLOR :
+        UIColor.whiteColor
+  end
+
+  # @param [Poem] poem
+  # @return [UITableViewCellEditingStyle]
+  def acc_type_for_poem(poem)
+    status100.of_number(poem.number) ?
+        UITableViewCellAccessoryCheckmark :
+        UITableViewCellAccessoryNone
+  end
+
+  # @param [Poem] poem
+  # @return [String]
+  def search_text_for_poem(poem)
+    [poem.liner, poem.in_hiragana, poem.in_modern_kana, poem.poet, poem.kimari_ji].
+        flatten.join(' ')
+  end
+
+end
--- app/screens/poem_picker.rb
+++ app/screens/poem_picker.rb
@@ -1,12 +1,13 @@
 class PoemPicker < PM::TableScreen
   include SelectedStatusHandler
   include OH::Notifications
+  include PoemPickerDataSource
 
-  SELECTED_BG_COLOR = '#eebbcb'.uicolor #撫子色
   title '歌を選ぶ'
   searchable placeholder: '歌を検索'
 
   attr_accessor :status100
+  attr_reader :table_search_display_controller
 
   def on_load
     init_members
@@ -17,31 +18,6 @@ class PoemPicker < PM::TableScreen
   def init_members
     self.status100 = loaded_selected_status
     self
-  end
-
-  def table_data
-    [{
-         cell_style: UITableViewCellStyleValue1,
-         cells: poems.map{|poem|
-           {
-               title: '%3d. %s %s %s %s %s' %
-                   ([poem.number] + (0..4).to_a.map{|idx| poem.liner[idx]}),
-
-               subtitle: "　　 #{poem.poet}",
-               search_text: search_text_for_poem(poem),
-               action: :poem_tapped,
-               arguments: {number: poem.number},
-               style: {
-                   # font: UIFont.fontWithName('HiraMinProN-W6', size: 16),
-                   font: UIFont.fontWithName('HiraMinProN-W6', size: MDT::Font.body.pointSize),
-                   background_color: bg_color_for_poem(poem),
-                   accessibility_label: '%03d' % poem.number,
-                   accessory_type: acc_type_for_poem(poem),
-               },
-               height: MDT::Font.body.pointSize * 3
-           }
-         }
-     }]
   end
 
   def poem_tapped(arg_hash)
@@ -60,7 +36,6 @@ class PoemPicker < PM::TableScreen
     prepare_text_field
     update_table_and_prompt
   end
-
 
   def will_disappear
     app_delegate.settings_manager.save
@@ -103,7 +78,7 @@ class PoemPicker < PM::TableScreen
   end
 
   def refresh_search_result_table
-    @table_search_display_controller.searchBar.text = @table_search_display_controller.searchBar.text
+    table_search_display_controller.searchBar.text = table_search_display_controller.searchBar.text
   end
 
   def select_all_poems
@@ -157,29 +132,6 @@ class PoemPicker < PM::TableScreen
   # @param [UITextField] text_field
   def set_text_field_japanese(text_field)
     text_field.keyboardType = UIKeyboardTypeDefault
-  end
-
-  # @param [Poem] poem
-  # @return [UIColor]
-  def bg_color_for_poem(poem)
-    status100.of_number(poem.number) ?
-        SELECTED_BG_COLOR :
-        UIColor.whiteColor
-  end
-
-  # @param [Poem] poem
-  # @return [UITableViewCellEditingStyle]
-  def acc_type_for_poem(poem)
-    status100.of_number(poem.number) ?
-        UITableViewCellAccessoryCheckmark :
-        UITableViewCellAccessoryNone
-  end
-
-  # @param [Poem] poem
-  # @return [String]
-  def search_text_for_poem(poem)
-    [poem.liner, poem.in_hiragana, poem.in_modern_kana, poem.poet, poem.kimari_ji].
-        flatten.join(' ')
   end
 
   def alert_ngram_picker_disabled
