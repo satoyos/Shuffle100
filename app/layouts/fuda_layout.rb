b41a44e954e568df3b6a435fc147fd537fa62ec9
Yoshifumi Sato <sato_yos@nifty.com>
2015-01-02 22:06:30 +0900

    Refactor: 意味の分かりにくい計算をメソッド分割し、メソッド名で説明するようにした。

--- app/layouts/fuda_layout.rb
+++ app/layouts/fuda_layout.rb
@@ -28,8 +28,17 @@ class FudaLayout < MK::Layout
 
   def fuda_height_on_me(ratio)
     return 300 unless top_offset
-    [(view.frame.size.height - top_offset) * ratio,
-        view.frame.size.width * ratio / FudaView::FUDA_SIZE_IN_MM.width * FudaView::FUDA_SIZE_IN_MM.height].min
+    [height_by_parent_height_with(ratio),
+     height_by_parent_width_with(ratio)].min
+  end
+
+  def height_by_parent_width_with(ratio)
+    view.frame.size.width * ratio /
+        FudaView::FUDA_SIZE_IN_MM.width * FudaView::FUDA_SIZE_IN_MM.height
+  end
+
+  def height_by_parent_height_with(ratio)
+    (view.frame.size.height - top_offset) * ratio
   end
 
 end
