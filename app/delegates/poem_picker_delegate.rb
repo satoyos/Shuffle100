78731fad8c5bcda4ebfd97390cbf7e643e1cd783
Yoshifumi Sato <sato_yos@nifty.com>
2015-01-08 20:19:47 +0900

    Add: debug puts to PoemPicker

--- app/delegates/poem_picker_delegate.rb
+++ app/delegates/poem_picker_delegate.rb
@@ -1,6 +1,7 @@
 module PoemPickerDelegate
 
   def poem_tapped(arg_hash)
+    puts "△ 歌番号#{arg_hash[:number]}番の選択状態を反転します。" if BW2.debug?
     status100.reverse_in_number(arg_hash[:number])
     update_table_and_prompt
     puts "searching? => #{searching?}" if BW2.debug?
@@ -11,9 +12,11 @@ module PoemPickerDelegate
   end
 
   def poem_long_pressed(arg_hash)
+    puts "▲ 歌番号#{arg_hash[:number]}番の取り札を表示します。" if BW2.debug?
     open_modal FudaScreen.new(nav_bar: true).tap{|s|
                  poem = poems[arg_hash[:number]-1]
                  s.fuda_str = poem.in_hiragana.shimo
+                 puts "→表示する下の句: #{s.fuda_str}" if BW2.debug?
                  s.nav_bar_title = poem.str_with_number_and_liner
                }
   end
