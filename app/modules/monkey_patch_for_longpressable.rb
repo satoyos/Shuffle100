454b1ff74d98cc5cf31823bfbfd5c48d23b89852
Yoshifumi Sato <sato_yos@nifty.com>
2015-01-10 22:22:15 +0900

    Fix: 歌の検索結果画面でスクロール後に歌を長押しすると、違う歌の取り札が表示される問題。( fix  #35 )
      - ProMotion::Table内で長押しされたセルを求める処理の中で、検索結果画面のスクロール量が反映されていなかった。
      - そのため、独自にスクロール量を求める処理を実装し、それによって「長押しされた位置」に補正をかけるようなモンキーパッチを書いた。
      - ProMotionの方でこの問題が解決されれば、これらの処理やモンキーパッチは不要になる予定。

--- app/modules/monkey_patch_for_longpressable.rb
+++ app/modules/monkey_patch_for_longpressable.rb
@@ -19,12 +19,26 @@ module ProMotion
         if searching?
           puts '!! contentOffset of searchResutsView => '
           ap table_search_display_controller.searchResultsTableView.contentOffset
+          gesture_point = point_on_scrolled_screen(gesture_point)
         end
         index_path = table_view.indexPathForRowAtPoint(gesture_point)
         puts '!! index_path => '
         ap index_path
+        return unless index_path # <= これ、pull requestしていいかも？？
         data_cell = self.promotion_table_data.cell(index_path: index_path)
         trigger_action(data_cell[:long_press_action], data_cell[:arguments], index_path) if data_cell[:long_press_action]
+      end
+
+      private
+
+      def point_on_scrolled_screen(org_point)
+        puts "- スクロールされている長さ(#{scroll_length})分だけgesture_pointを補正します。" if BW2.debug?
+        CGPointMake(org_point.x, org_point.y + scroll_length)
+      end
+
+      def scroll_length
+        table_search_display_controller.searchResultsTableView.contentOffset.y -
+            @searching_offset_y
       end
     end
 
