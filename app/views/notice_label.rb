c803a98d9404b61cda8b94403b1e99024bdcd5fc
Yoshifumi Sato <sato_yos@nifty.com>
2015-03-13 22:39:58 +0900

    Remove Helper Class

--- app/views/notice_label.rb
+++ app/views/notice_label.rb
@@ -1,3 +1,11 @@
 class NoticeLabel < UILabel
-
+  def move_up_half_height
+    f = self.frame
+    self.frame =
+        CGRectMake(f.origin.x,
+                   f.origin.y - f.size.height / 2,
+                   f.size.width,
+                   f.size.height
+        )
+  end
 end
