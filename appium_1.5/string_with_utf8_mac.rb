60478b555130d50cc169e0ff7d276fd7f374c8f9
Yoshifumi Sato <sato_yos@nifty.com>
2016-08-28 17:02:27 +0900

    Remove tempraly created folder for Appium 1.5 migration

--- appium_1.5/string_with_utf8_mac.rb
+++
@@ -1,9 +0,0 @@
-module StringWithUTF8Mac
-  def ==(other)
-    self.encode('UTF-8', 'UTF-8-Mac').eql? other.encode('UTF-8', 'UTF-8-Mac')
-  end
-end
-
-class String
-  prepend StringWithUTF8Mac
-end
