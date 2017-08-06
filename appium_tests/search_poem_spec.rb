48b13925fa31ee75e3c12a63acf85a864eddc3c6
Yosifumi Sato <sato_yos@nifty.com>
2017-08-06 22:24:51 +0900

    Update search_poem_spec, and now 5 tests pass! for Appium 1.6.5
         - open_help_spec
         - beginner_mode_spec
         - on_game_settings_spec
         - quit_game_spec
         - search_poem_spec <= NEW!!

--- appium_tests/search_poem_spec.rb
+++ appium_tests/search_poem_spec.rb
@@ -9,15 +9,16 @@ describe '歌を検索するテスト' do
 
   describe '検索窓に文字を入力すると、歌を絞り込むことができる' do
     it '歌選択画面を開く' do
-      click_element_of('UIATableCell', name: '取り札を用意する歌')
+      click_element_with_text('取り札を用意する歌')
       can_see('歌を選ぶ')
     end
-    it '検索窓に「秋」を入力すると、1番, 5番, 22番…という順に歌が選ばれている。' do
+    it '検索窓に「秋」を入力すると、1番, 5番, 22番…という順に歌が選ばれていて、2番は選ばれない。' do
       find_element(:accessibility_id, "search_text_field").send_keys "秋"
-      cell_names = find_elements(:class_name, 'UIATableCell').map{|e| e.name}
+      cell_names = find_elements(:class_name, 'XCUIElementTypeCell').map{|e| e.name}
       expect(cell_names.first).to eq '001'
       expect(cell_names[1]).to eq '005'
       expect(cell_names[2]).to eq '022'
+      expect(cell_names.include? '002').to be false
     end
   end
 end
