ecd3e21afa4be8a9bfde19190d2164a5c1fe5c0c
Yosifumi Sato <sato_yos@nifty.com>
2017-08-14 17:31:03 +0900

    Enable Slack notification when Appium test finished

--- appium_tests/lib/slack_formatter.rb
+++ appium_tests/lib/slack_formatter.rb
@@ -4,6 +4,8 @@ require 'slack-notifier'
 RSpec::Support.require_rspec_core 'formatters/base_formatter'
 
 class SlackFormatter < RSpec::Core::Formatters::BaseFormatter
+  attr_reader :results
+
   RSpec::Core::Formatters.register self, :example_passed, :example_failed
 
   def initialize(output)
@@ -20,20 +22,21 @@ class SlackFormatter < RSpec::Core::Formatters::BaseFormatter
   end
 
   def  close(notification)
-    post_alack
+    post_slack
   end
 
-  def post_slack
+    def post_slack
     notifier = Slack::Notifier.new(ENV['SLACK_DEV_WEBHOOK_URL'])
+    note = {}
 
     if @results.include? 'NG'
       note = {
-          text: 'ダメー',
+          text: ':scream: 百読のAppiumテストで異状あり！',
           color: 'warning'
       }
     else
       note = {
-          text: 'おっけー',
+          text: "百読のAppiumテスト、#{@results.size}個のテストが全部成功です！ :thumbsup:",
           color: 'good'
       }
     end
@@ -41,3 +44,15 @@ class SlackFormatter < RSpec::Core::Formatters::BaseFormatter
     notifier.post text: '', attachments: [note]
   end
 end
+
+# 以下、上記クラスの動作確認用コード！
+
+if $0 == __FILE__
+  formatter = SlackFormatter.new(nil)
+  formatter.example_passed(nil)
+  formatter.example_passed(nil)
+  # formatter.example_failed(nil)
+  puts "@results => #{formatter.results}"
+  formatter.post_slack
+end
+
