f6a2dc9c352a8b79e22e76abe63705a6acc37f15
Yosifumi Sato <sato_yos@nifty.com>
2017-10-21 20:14:10 +0900

    Switch badge icon (selected poems num inticator) by iOS Version
      - move position if iOS Ver >= 11

--- app/views/poems_number_badge_item.rb
+++ app/views/poems_number_badge_item.rb
@@ -4,6 +4,7 @@ class PoemsNumberSelectedItem < BBBadgeBarButtonItem
     def create_with_origin_x(org_x)
       self.alloc.initWithCustomUIButton(UIButton.alloc.init).tap do |bb|
         bb.badgeOriginX = org_x
+        bb.badgeOriginY = 0.0  if BW2.ios_major_ver_num >= 11
         bb.shouldHideBadgeAtZero = false
       end
     end
