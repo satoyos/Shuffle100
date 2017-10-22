edc2aca70c58452e116900223f3a802e9034db5f
Yosifumi Sato <sato_yos@nifty.com>
2017-10-22 19:08:36 +0900

    Update spec_helper for Appium tests;
      - supports iOS version >= 10 only (cut out iOS 9)

--- appium_tests/spec_helper.rb
+++ appium_tests/spec_helper.rb
@@ -17,8 +17,9 @@ def desired_caps
           # versionNumber: "9.2",
           deviceName:    "iPhone 5s",
           platformVersion: "11.0",
+          # platformVersion: "10.3",
           # deviceName:    "iPad Pro",
-          app: '../build/iPhoneSimulator-9.0-Development/Shuffle100.app',
+          app: '../build/iPhoneSimulator-10.0-Development/Shuffle100.app',
           # fullReset: true,  # Appium1.5+(試したのは1.5.3)で、シミュレータの2回起動を抑止する。
           automationName: 'XCUITest'
       },
