d71ab5389246f1dd8259784cd5b46bee48cfa50c
Yosifumi Sato <sato_yos@nifty.com>
2018-05-24 23:17:06 +0900

    Implement NSCoding protocol for FudaSet to save FudaSet data

--- app/models/fuda_set.rb
+++ app/models/fuda_set.rb
@@ -1,10 +1,26 @@
 class FudaSet
-  attr_reader :name, :status100
+  attr_accessor :name, :status100
+
+  KEY_FUDA_SET_NAME = 'fuda_set_name'
 
   def initialize(name, status100)
     raise '札セットの初期化時に名前を文字列で指定してください' if name.nil? || !name.is_a?(String)
     raise '札セットの初期化時に、札の構成をBoolenanの配列で指定してください' if status100.nil? || !status100.is_a?(SelectedStatus100)
     @name = name
     @status100 = status100
+  end
+
+  # NSCoding Protocol
+
+  def initWithCoder(decoder)
+    @status100 = decoder.decodeObjectForKey(SelectedStatus100::KEY_STATUS)
+    @name = decoder.decodeObjectForKey(KEY_FUDA_SET_NAME)
+    self
+  end
+
+  def encodeWithCoder(encoder)
+    # puts 'ヽ(`Д´)ﾉ エンコーダに入ったからね！！' if BW2.debug?
+    encoder.encodeObject(self.status100, forKey: SelectedStatus100::KEY_STATUS)
+    encoder.encodeObject(self.name, forKey: KEY_FUDA_SET_NAME)
   end
 end
--- spec/models/fuda_set_spec.rb
+++ spec/models/fuda_set_spec.rb
@@ -1,19 +1,35 @@
-
-
 describe FudaSet do
   ODD_SET_STATUS  = (1..100).map{|n| n % 2 == 1 ? true : false}
   EVEN_SET_STATUS = (1..100).map{|n| n % 2 == 0 ? true : false}
-  before do
-    @set = FudaSet.new('偶数番の札', SelectedStatus100.new(EVEN_SET_STATUS))
-    # @set = FudaSet.new
+  describe '初期化' do
+    before do
+      @set = FudaSet.new('偶数番の札', SelectedStatus100.new(EVEN_SET_STATUS))
+      # @set = FudaSet.new
+    end
+    it 'should be a valid object' do
+      @set.should.not.be.nil
+      @set.is_a?(FudaSet).should.be.true
+    end
+    it 'プロパティがきちんと設定されている' do
+      @set.name.should == '偶数番の札'
+      @set.status100.is_a?(SelectedStatus100).should.be.true
+      @set.status100.selected_num.should == 50
+    end
   end
-  it 'should be a valid object' do
-    @set.should.not.be.nil
-    @set.is_a?(FudaSet).should.be.true
-  end
-  it 'プロパティがきちんと設定されている' do
-    @set.name.should == '偶数番の札'
-    @set.status100.is_a?(SelectedStatus100).should.be.true
-    @set.status100.selected_num.should == 50
+
+  describe 'NSCodingプロトコル' do
+    before do
+      @set = FudaSet.new('偶数番の札', SelectedStatus100.new(EVEN_SET_STATUS))
+      @data_to_save = NSKeyedArchiver.archivedDataWithRootObject(@set)
+    end
+    it 'セーブ対象データを作成できる' do
+      @data_to_save.should.not.be.nil
+    end
+    it 'セーブ対象データからオブジェクトを復元できる' do
+      loaded_set =  NSKeyedUnarchiver.unarchiveObjectWithData(@data_to_save)
+      loaded_set.is_a?(FudaSet).should.be.true
+      loaded_set.name.should == @set.name
+      loaded_set.status100.selected_num.should == 50
+    end
   end
 end
