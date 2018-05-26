6079bf1a9c9c6788e90803eba6e901cd3ae553a4
Yosifumi Sato <sato_yos@nifty.com>
2018-05-26 22:39:24 +0900

    Update .gitignore: igonre png file in appium_tests/screenshots/

--- spec/models/game_settings_spec.rb
+++ spec/models/game_settings_spec.rb
@@ -37,6 +37,11 @@ describe 'GameSettings' do
     it 'recite_modeを管理' do
       @settings.recite_mode_id.should == :normal
     end
+
+    it 'fuda_setsを管理' do
+      @settings.fuda_sets.is_a?(Array).should.be.true
+      @settings.fuda_sets.size.should == 0
+    end
   end
 
   describe '#set_recite_mode' do
@@ -51,6 +56,28 @@ describe 'GameSettings' do
           s.beginner_flg.should == false
           s.recite_mode_id.should == :normal
         end
+      end
+    end
+  end
+
+  describe 'NSCodingプロトコル' do
+    before do
+      @settings = GameSettings.new
+      test_status100 = SelectedStatus100.new(true)
+      test_fuda_set = FudaSet.new('テスト1', test_status100)
+      @settings.fuda_sets.push(test_fuda_set)
+      @data_to_save = NSKeyedArchiver.archivedDataWithRootObject(@settings)
+    end
+    it 'セーブ対象データを作成できる' do
+      @data_to_save.should.not.be.nil
+    end
+    it 'セーブ対象データからオブジェクトを復元できる' do
+      loaded_settings =  NSKeyedUnarchiver.unarchiveObjectWithData(@data_to_save)
+      loaded_settings.is_a?(GameSettings).should.be.true
+      loaded_settings.fuda_sets.tap do |sets|
+        sets.size.should == 1
+        sets[0].name.should == 'テスト1'
+        sets[0].status100.is_a?(SelectedStatus100).should.be.true
       end
     end
   end
