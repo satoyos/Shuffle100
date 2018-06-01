6552b1c071174cd6ae2e1efd6921e4615518a618
Yosifumi Sato <sato_yos@nifty.com>
2018-06-01 21:47:49 +0900

    Adapt update of Xcode (9.3 -> 9.4), iOS (11.3 -> 11.4)

--- appium_tests/spec_helper.rb
+++ appium_tests/spec_helper.rb
@@ -24,7 +24,7 @@ def desired_caps
           deviceName:    "iPhone X",
           # deviceName:    "iPhone 7 Plus",
           # deviceName:    "iPad Pro (12.9-inch)",
-          platformVersion: "11.3",
+          platformVersion: "11.4",
           app: '../build/iPhoneSimulator-11.0-Development/Shuffle100.app',
           # fullReset: true,  # Appium1.5+(試したのは1.5.3)で、シミュレータの2回起動を抑止する。
           automationName: 'XCUITest'
