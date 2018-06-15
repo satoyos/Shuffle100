ca9014f1f216aa78d9e1b98ae859311e6570c59f
Yosifumi Sato <sato_yos@nifty.com>
2018-06-15 22:20:18 +0900

    set conditional wait in first appuum test script

--- appium_tests/beginner_mode_spec.rb
+++ appium_tests/beginner_mode_spec.rb
@@ -4,6 +4,8 @@ require_relative 'spec_helper'
 describe '初心者モードのテスト' do
 
   it 'アプリのタイトルが正しく表示される' do
+    wait = Selenium::WebDriver::Wait.new :timeout => 60
+    wait.until { elems_of_str('トップ').first.displayed? }
     can_see(TITLE)
   end
 
