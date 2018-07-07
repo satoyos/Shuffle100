7c9b4cc407a7220f873b90ee5c344eebf05d99c4
Yosifumi Sato <sato_yos@nifty.com>
2018-07-07 22:34:02 +0900

    [WIP] add appium test to move FudaSetScreen, but it fails.
     - Because the expected screen is not implemented.

--- appium_tests/fuda_set_spec.rb
+++ appium_tests/fuda_set_spec.rb
@@ -37,9 +37,22 @@ describe '札セットを保存するテスト' do
       click_new_set_button
       can_see '新しい札セットの名前'
     end
+=begin
     it '新しい札セットの名前入力画面でキャンセルを押すと、札選択画面に戻る' do
       click_cancel_button
       current_screen_is STR_SELECT_POEM_SCREEN
+    end
+    it 'もう一度、命名画面を表示' do
+      click_save_button
+      sleep_while_animation
+      click_new_set_button
+      can_see '新しい札セットの名前'
+    end
+=end
+    it '名前を入力し、「決定」を押すと、札セット一覧画面に繊維する' do
+      fill_text_field_with '2字決まりセット'
+      click_fix_button
+      can_see '作った札セット'
     end
   end
 end
@@ -56,4 +69,13 @@ end
 def click_cancel_button
   sleep_while_animation
   click_button(STR_CANCEL)
+end
+
+def click_fix_button
+  sleep_while_animation
+  click_button('決定')
+end
+
+def fill_text_field_with(str)
+  find_element(:accessibility_id, 'name_field').send_keys str
 end
