c5f37f8ea85316f51a964b2740949093345b1840
Yosifumi Sato <sato_yos@nifty.com>
2018-08-05 20:30:58 +0900

    Refactor: remove unused code (already comment out)

--- spec/layouts/name_new_set_layout_spec.rb
+++ spec/layouts/name_new_set_layout_spec.rb
@@ -19,27 +19,6 @@ describe 'NameNewSetLayout' do
       @layout.get(:cancel_button).should.not.be.nil
       @layout.get(:cancel_button).is_a?(UIButton).should.be.true
     end
-=begin
-    it '背景色は白' do
-      @layout.view.backgroundColor.should == UIColor.whiteColor
-    end
-
-    it '各ボタンにAccessibilityLabelが付与されている' do
-      [:play_button, :quit_button, :gear_button].each do |button_sym|
-        @layout.get(button_sym).accessibilityLabel.should == button_sym.to_s
-      end
-      [:forward, :rewind].each do |button_sym|
-        @layout.get("#{button_sym}_button".to_sym).accessibilityLabel.should ==
-            button_sym.to_s
-      end
-    end
-    it 'notice_labelが設定されている' do
-      @layout.get(:notice_label).tap do |n_label|
-        n_label.should.not.be.nil
-        n_label.backgroundColor.should == RecitePoemStyles::NOTICE_LABEL_BACKCOLOR
-      end
-    end
-=end
   end
 
 end
