1612be51f0db8f0724020d282ca12f3299944b04
Yosifumi Sato <sato_yos@nifty.com>
2018-08-11 15:24:43 +0900

    Add appium test to delete fuda set

--- appium_tests/fuda_set_spec.rb
+++ appium_tests/fuda_set_spec.rb
@@ -12,11 +12,40 @@ STR_NAME_NOT_DEFINED = '新しい札セットの名前を決めましょう'
 STR_SAVE_COMPLETE = '保存完了'
 STR_OVER_WRITE_COMPLETE = '上書き保存完了'
 STR_OVERWRITE_SET = '前に作った札セットに上書きする'
+STR_DELETE_FUDA_SET = '削除'
 
 shared_examples '歌を選ぶ画面を開く' do
   it '歌選択画面を開く' do
     goto_select_poem_screen
     current_screen_is STR_SELECT_POEM_SCREEN
+  end
+end
+
+shared_examples '「1字決まりセット」を作る' do
+  include_examples '歌を選ぶ画面を開く'
+  it '選択状態をオールクリア' do
+    click_button_to_cancel_all
+  end
+  it '「1字目で選ぶ」画面に移動' do
+    sleep_while_animation
+    select_by_group
+    open_first_char_select_screen
+  end
+  it '1字決まりの歌を選んで戻る' do
+    click_element_of(TYPE_CELL, name: 'just_one')
+    click_back_button
+  end
+  it '新しい札セット「１字決まりのセット」として保存' do
+    click_save_button
+    click_new_set_button
+    fill_name_field_with STR_TEST_SET_NAME_1C
+    click_fix_button
+    can_see STR_SAVE_COMPLETE
+    alert_accept
+  end
+  it 'ホーム画面に戻り、枚数確認' do
+    click_back_button
+    can_see '7首'
   end
 end
 
@@ -86,31 +115,7 @@ describe '札セットを保存するテスト' do
     end
     describe '札セットの上書きテスト' do
       describe '「1字決まりの札セット」作る' do
-        include_examples '歌を選ぶ画面を開く'
-        it '選択状態をオールクリア' do
-          click_button_to_cancel_all
-        end
-        it '「1字目で選ぶ」画面に移動' do
-          sleep_while_animation
-          select_by_group
-          open_first_char_select_screen
-        end
-        it '1字決まりの歌を選んで戻る' do
-          click_element_of(TYPE_CELL, name: 'just_one')
-          click_back_button
-        end
-        it '新しい札セット「１字決まりのセット」として保存' do
-          click_save_button
-          click_new_set_button
-          fill_name_field_with STR_TEST_SET_NAME_1C
-          click_fix_button
-          can_see STR_SAVE_COMPLETE
-          alert_accept
-        end
-        it 'ホーム画面に戻り、枚数確認' do
-          click_back_button
-          can_see '7首'
-        end
+        include_examples '「1字決まりセット」を作る'
       end
       describe '札選択画面に移動し、1首めを選んでみる' do
         include_examples '歌を選ぶ画面を開く'
@@ -159,6 +164,35 @@ describe '札セットを保存するテスト' do
   end
 end
 
+describe '札セットはスワイプで削除できる' do
+  include_examples '「1字決まりセット」を作る'
+  include_examples '歌を選ぶ画面を開く'
+  it '「まとめて選ぶ」を押すと、「作った札セットから選ぶ」アクションが表示されている' do
+    select_by_group
+    can_see STR_SELECT_FROM_FUDA_SETS
+  end
+  it '「作った札セットから選ぶ」をタップすると、札セット一覧画面に遷移する' do
+    open_fuda_set_list_screen
+  end
+  it '作ったばかりの札セットを確認できる' do
+    can_see STR_TEST_SET_NAME_1C
+    e = elems_of_str(STR_TEST_SET_NAME_1C).first
+    puts "該当エレメントの情報: [#{e.location.x}, #{e.location.y}], [#{e.size.width}, #{e.size.height}]"
+    swipe_elem(e, 60)
+    can_see STR_DELETE_FUDA_SET
+  end
+  it '削除ボタンを押すと、その札セットがリストから削除される' do
+    click_button STR_DELETE_FUDA_SET
+    sleep_while_animation
+    can_not_see STR_TEST_SET_NAME_1C
+  end
+  it '歌選択画面で再度「まとめて選ぶ」を選んでも、札セットが0個のため「作った札セットから選ぶ」が表示されない' do
+    click_back_button
+    select_by_group
+    can_not_see STR_SELECT_FROM_FUDA_SETS
+  end
+end
+
 def click_save_button
   click_button(STR_SAVE_FUDA_SET)
 end
@@ -185,4 +219,16 @@ end
 def select_one_char_set
   wheel = find_element(class_name: TYPE_PICKER_WHEEL)
   wheel.send_keys STR_TEST_SET_NAME_1C
+end
+
+def swipe_elem(element, swipe_left_length)
+  puts "↑↑ #{swipe_left_length}だけ左にスワイプします"
+  startX = element.location.x + element.size.width / 2
+  startY = element.location.y + element.size.height / 2
+  Appium::TouchAction.new.swipe(
+      start_x: startX,
+      start_y: startY,
+      end_x: startX - swipe_left_length,
+      end_y: startY,
+      duration: 1000).perform
 end
