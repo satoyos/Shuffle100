233775a17d4edceffc1e98e3bfe1e96f2e9a4738
Yosifumi Sato <sato_yos@nifty.com>
2018-08-27 21:46:05 +0900

    Cut out BW2 class from app_delegate file

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -66,26 +66,3 @@ class AppDelegate < PM::Delegate
     @sizes = OH::DeviceSizeManager.select_sizes
   end
 end
-
-class BW2
-  def self.retina_ratio
-    UIScreen.mainScreen.scale
-  end
-
-  def self.debug=(value)
-    @debug_flag = value
-  end
-
-  def self.debug?
-    @debug_flag ||= false
-  end
-
-  def self.ios_version
-    @ios_version ||= UIDevice.currentDevice.systemVersion
-  end
-
-  def self.ios_major_ver_num
-    @ios_major_ver_num ||= ios_version.split(/\./).first.to_i
-  end
-
-end
